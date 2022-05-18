648ee688cd2b355fdff298eabc6efd97b969def3
Sathyapriyamca <98675170+Sathyapriyamca@users.noreply.github.com>
2022-05-18 08:49:43 +0530

    Mobile.java

---
+++ Mobile.java
@@ -0,0 +1,21 @@
+package FirstWeek;
+
+public class Mobile {
+	public void Makecall() 
+	{
+		System.out.println("Make call");
+	}
+	public void SendMsg()
+	{
+		System.out.println("Send Msg");
+	}
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mobile myMobile = new Mobile();
+		myMobile.Makecall();
+		myMobile.SendMsg();
+
+	}
+
+}
